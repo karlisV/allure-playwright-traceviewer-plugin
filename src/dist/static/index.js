cecdf44ae16007aab0aa39d19fdf56c30cb960bb
Karlis <karlis.vejs@gmail.com>
2024-05-13 08:56:22 +0300

    add trace tab in test result section

--- src/dist/static/index.js
+++ src/dist/static/index.js
@@ -1,8 +1,15 @@
 'use strict';
 
-const settings = allure.getPluginSettings('playwright-traceview-plugin', {default_trace_app_url: "https://playwright.aqa.forte.io"});
+const TRACE_VIEW_APP_URL = "https://trace.playwright.dev"
 
-    function getBasePath() {
+const TraceView = Backbone.Marionette.View.extend({
+
+    
+    initialize: function (data) {
+        this.data = data;
+    },
+
+    getBasePath: () => {
         let pathname = window.location.pathname;
         if (pathname.includes('/')) {
             const segments = pathname.split('/');
@@ -15,99 +22,44 @@ const settings = allure.getPluginSettings('playwright-traceview-plugin', {defaul
             pathname += '/';
         }
         return window.location.protocol + '//' + window.location.host + pathname;
-    }
+    },
 
-    function findTraceAttachment(data) {
-        for(const stage of data.afterStages) {
-            if (stage.attachments.length > 0){
-                return stage.attachments.find(att => att.name === "trace-view");
-            }
-        }
-    }
+    findTraceAttachment: (attachments) => {
+        return attachments.find(att => att.name === "pw-trace");
+    },
 
-    const TraceAttachmentView = Backbone.Marionette.View.extend({
 
-        initialize: function (attachment) {
-            this.data = attachment;
-        },
-
-        templateContext: function() {
-            return {
-                traceViewUrl: settings.get("default_trace_app_url"),
-                attachment: this.data
-            };
-        },
-
-        template: function(data) {
-            const attachmentLink = `${getBasePath()}data/attachments/${data.attachment.source}`;
-            const pwTraceViewBaseUrl = data.traceViewUrl;
+    template: function(data) {
+        const att = this.findTraceAttachment(data.model.allAttachments);
+        if (att) {
+            const attachmentLink = `${this.getBasePath()}data/attachments/${att.source}`;
+            const pwUrl = `${TRACE_VIEW_APP_URL}/?trace=${attachmentLink}`;
+            console.log(pwUrl);
             return `<div class="trace-view-container">
-                        <h4>Trace Data Visualization</h4>
-                        <button class="open-trace-button" data-url="${pwTraceViewBaseUrl}?trace=${attachmentLink}">OPEN TRACE VIEWER</button>
-                        <div class="trace-content"></div>
-                    </div>`;
-        },
-
-        events: {
-            'click .open-trace-button': 'openTraceViewer'
-        },
-
-        // Modify the openTraceViewer function to create and append an iframe to the desired location
-        openTraceViewer: function(event) {
-            event.preventDefault();
-            const traceUrl = event.currentTarget.dataset.url;
-            const traceIframe = document.createElement('iframe');
-            traceIframe.src = traceUrl;
-            traceIframe.width = '100%';
-            traceIframe.height = '600px';
-            traceIframe.frameBorder = '0';
-            // Append the iframe to the test-result-overview div
-            const testResultOverview = document.querySelector('.test-result-overview');
-            testResultOverview.appendChild(traceIframe);
+            <a href="${pwUrl}" target="_blank">Open trace viewer in new tab</a>
+            <div class="trace-content">
+            <iframe src="${pwUrl}" width="100%" height="800px" frameborder="0" sandbox="allow-same-origin allow-scripts allow-popups allow-forms"></iframe>
+            </div>
+            </div>`
         }
+        return `<div class="trace-view-container"><p>no trace to load</p></div>` 
+;
+    },
 
-    });
 
-    const TestResultTraceView = Backbone.Marionette.View.extend({
-        regions: {
-            subView: '.trace-view-container',
-        },
-        template: function() {
-            return '<div class="trace-view-container"></div>';
-        },
-        onRender: function() {
-            const data = this.model.toJSON();
-            const attachment = findTraceAttachment(data);
-            if (attachment) {
-                this.showChildView('subView', new TraceAttachmentView(attachment));
-            }
-        },
-    });
+    render: function () {
+        this.$el.html(this.template(this.options));
+        return this;
+    },
+});
 
-    allure.api.addTestResultBlock(TestResultTraceView, { position: 'after' });
-})();
 
 allure.api.addTranslation('en', {
-    tab: {
-        traces: {
-            name: 'Traces'
+    testResult: {
+        trace: {
+            name: 'Trace'
         }
     }
 });
 
-allure.api.addTab('traces', {
-    title: 'tab.traces.name', icon: 'fa fa-eye',
-    //below structure is similar to behaviours, need to understand how this works
-    route: 'traces(/)(:testGroup)(/)(:testResult)(/)(:testResultTab)(/)',
-    onEnter: (function (testGroup, testResult, testResultTab) {
-        return new allure.components.TreeLayout({
-            testGroup: testGroup,
-            testResult: testResult,
-            testResultTab: testResultTab,
-            tabName: 'tab.traces.name',
-            baseUrl: 'traces',
-            url: 'data/traces.json',
-            csvUrl: 'data/traces.csv'
-        });
-    })
-});
+allure.api.addTestResultTab("trace", "testResult.trace.name", TraceView);
