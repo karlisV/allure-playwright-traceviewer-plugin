8fd507e1b2e783cd14278a54fe73f7b389c2623c
emreForte <emreaman@forte.io>
2024-05-30 06:54:25 +0300

    changed spinner design

--- src/dist/static/index.js
+++ src/dist/static/index.js
@@ -1,83 +1,88 @@
-'use strict';
+"use strict";
 
-const TRACE_VIEW_APP_URL = "https://trace.playwright.dev"
+const TRACE_VIEW_APP_URL = "https://trace.playwright.dev";
 
 const TraceView = Backbone.Marionette.View.extend({
-    initialize: function(data) {
-        this.data = data;
-        this.fetchTraceContent();
-    },
+  initialize: function (data) {
+    this.data = data;
+    this.fetchTraceContent();
+  },
 
-    getBasePath: function() {
-        let pathname = window.location.pathname;
-        if (pathname.includes('/')) {
-            const segments = pathname.split('/');
-            const lastSegment = segments.pop();
-            if (lastSegment.includes('.')) {
-                pathname = segments.join('/');
-            }
-        }
-        if (!pathname.endsWith('/')) {
-            pathname += '/';
-        }
-        return window.location.protocol + '//' + window.location.host + pathname;
-    },
+  getBasePath: function () {
+    let pathname = window.location.pathname;
+    if (pathname.includes("/")) {
+      const segments = pathname.split("/");
+      const lastSegment = segments.pop();
+      if (lastSegment.includes(".")) {
+        pathname = segments.join("/");
+      }
+    }
+    if (!pathname.endsWith("/")) {
+      pathname += "/";
+    }
+    return window.location.protocol + "//" + window.location.host + pathname;
+  },
 
-    findTraceAttachment: function(attachments) {
-        return attachments.find(att => att.name === "pw-trace");
-    },
+  findTraceAttachment: function (attachments) {
+    return attachments.find((att) => att.name === "pw-trace");
+  },
 
-    fetchTraceContent: async function() {
-        const att = this.findTraceAttachment(this.data.model.allAttachments);
-        if (att && att.type.includes("text")) {
-            const attachmentLink = `${this.getBasePath()}data/attachments/${att.source}`;
-            try {
-                const response = await fetch(attachmentLink);
-                this.traceContent = await response.text();
-            } catch (error) {
-                console.error('Error fetching the attachment content:', error);
-            }
-            this.render();
-        } else if (att && att.type.includes("zip")){
-            this.traceContent = `${this.getBasePath()}data/attachments/${att.source}`;
-            this.render();
-        }
-    },
+  fetchTraceContent: async function () {
+    const att = this.findTraceAttachment(this.data.model.allAttachments);
+    if (att && att.type.includes("text")) {
+      const attachmentLink = `${this.getBasePath()}data/attachments/${
+        att.source
+      }`;
+      try {
+        const response = await fetch(attachmentLink);
+        this.traceContent = await response.text();
+      } catch (error) {
+        console.error("Error fetching the attachment content:", error);
+      }
+      this.render();
+    } else if (att && att.type.includes("zip")) {
+      this.traceContent = `${this.getBasePath()}data/attachments/${att.source}`;
+      this.render();
+    }
+  },
 
-    template: function() {
-        if (this.traceContent) {
-            const pwUrl = `${TRACE_VIEW_APP_URL}/?trace=${this.traceContent}`;
-            return `<div class="trace-view-container">
+  template: function () {
+    if (this.traceContent) {
+      const pwUrl = `${TRACE_VIEW_APP_URL}/?trace=${this.traceContent}`;
+      return `<div class="trace-view-container">
                 <a href="${pwUrl}" target="_blank">Open trace viewer in new tab</a>
                 <div class="trace-content">
-                <div class="spinner" id="trace-spinner"></div>
-                    <iframe src="${pwUrl}" width="100%" height="800px" frameborder="0" sandbox="allow-same-origin allow-scripts allow-popups allow-forms"></iframe>
-                </div>
-            </div>`;
-        }
-        return `<div class="trace-view-container"><p>No trace attached</p></div>`;
-    },
+                  <div class="loader" id='loading-spinner'>
+                    <div class="loader-text">loading</div>
+                  </div>
+                  <iframe id="trace-iframe" width="100%" height="800px" frameborder="0" sandbox="allow-same-origin allow-scripts allow-popups allow-forms"></iframe>
+                </div>`;
+    }
+    return `<div class="trace-view-container"><p>No trace attached</p></div>`;
+  },
 
-    render: function() {
-        this.$el.html(this.template());
-        const iframe = this.$el.find('iframe');
+  render: function () {
+    this.$el.html(this.template());
+    const iframe = this.$el.find("#trace-iframe");
 
-        if (iframe.length) {
-            iframe.on('load', function() {
-                document.getElementById('trace-spinner').style.display = 'none';
-            });
-        }
+    if (iframe.length) {
+      iframe.attr("src", `${TRACE_VIEW_APP_URL}/?trace=${this.traceContent}`);
+      iframe.on("load", function () {
+        document.getElementById("loading-spinner").style.display = "none";
+        iframe.css("display", "block");
+      });
+    }
 
-        return this;
-    },
+    return this;
+  },
 });
 
-allure.api.addTranslation('en', {
-    testResult: {
-        trace: {
-            name: 'Trace'
-        }
-    }
+allure.api.addTranslation("en", {
+  testResult: {
+    trace: {
+      name: "Trace",
+    },
+  },
 });
 
 allure.api.addTestResultTab("trace", "testResult.trace.name", TraceView);
